53a3851a4dfe4387999f608d24068095d07b133d
LinearYoung <1459440013@qq.com>
2025-09-17 21:21:16 +0800

    finish the lab2

--- Lab/lab02/lfsr.c
+++ Lab/lab02/lfsr.c
@@ -5,6 +5,13 @@
 #include "lfsr.h"
 
 void lfsr_calculate(uint16_t *reg) {
+    uint16_t bit0 = *reg & 1;
+    uint16_t bit2 = (*reg >> 2) & 1;
+    uint16_t bit3 = (*reg >> 3) & 1;
+    uint16_t bit5 = (*reg >> 5) & 1;
+
+    uint16_t add_val = bit0 ^ bit2 ^ bit3 ^ bit5;
+    *reg = (*reg >> 1) | (add_val << 15);
     /* YOUR CODE HERE */
 }
 
